56076083b8151a3b74c4a3d079e0f82510277021
Asciugano <samuele.diaferio@fastwebnet.it>
2025-09-23 20:17:26 +0200

    aggiunto il numero di corsi che l utente segue

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,17 +12,37 @@ export default async function ProfilePage() {
   if (!user)
     notFound();
 
+  const numberOfCourse = await prisma.course.count({
+    where: {
+      OR: [
+        {
+          enrollments: {
+            some: { userId }
+          },
+        },
+        {
+          teacherId: userId,
+        }
+      ],
+    },
+  });
+
   return (
     <div>
       <div className="flex items-center justify-center bg-neutral-200 dark:bg-neutral-800 rounded-lg shadow w-full px-4 py-2">
         {/* user information */}
-        <div>
+        <div className="text-center">
           <h2 className="text-lg font-semibold text-center">
             Benvenuto <span className="text-2xl font-bold text-amber-400">{user.username}</span>
           </h2>
 
           <p className="mt-8 text-neutral-500 dark:text-neutral-400 text-sm">La tua email:
             <span className="text-sm text-amber-400 hover:underline">{user.email}</span>
+          </p>
+
+          <p className="mt-8 text-sm">Sei inscritto a{" "}
+            <span className="text-amber-400">{numberOfCourse}</span>
+            {" "}Corsi
           </p>
         </div>
       </div>
